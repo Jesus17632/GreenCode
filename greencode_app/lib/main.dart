61fec654a6e94a593bf7d69652941c525dbee51b
Jesus17632 <jesusflores.tech@gmail.com>
2026-04-26 09:36:05 -0600

    linea

--- greencode_app/lib/main.dart
+++ greencode_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'Views/welcome_view.dart'; 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env"); // ← Carga la key
